f906a02b666989ebe03c85a31ae263b97497a4d2
Leon Hassan <leonhassan@hotmail.com>
2017-04-22 13:57:11 +0100

    Forgot to commit landing page js
    it locally saves the itinerary data, wipes when a new location is submitted

---
+++ app/js/landing.js
@@ -0,0 +1,58 @@
+'use strict';
+
+function initialise_autocomplete() {
+    let input = document.getElementById('location_input');
+    let autocomplete = new google.maps.places.Autocomplete(input);
+}
+
+var location_sent = false;
+
+function submit_location() {
+  localStorage.clear();
+  let location = document.getElementById('location_input').value;
+  location_sent = true;
+  console.log(location);
+  swap_location_buttons();
+  send_location(location);
+};
+
+function swap_location_buttons() {
+  if (location_sent) {
+    //show loading
+    let submit_button = document.getElementById('submit_button');
+    submit_button.style.display = 'none';
+    
+    let loading_div = document.getElementById('loading_div');
+    loading_div.style.display = 'block';
+    
+  } else {
+    //show not loading
+    let loading_div = document.getElementById('loading_div');
+    loading_div.style.display = 'none';
+    
+    let submit_button = document.getElementById('submit_button');
+    submit_button.style.display = 'block';
+    
+  }
+}
+
+function send_location(location) {
+  let my_url = 'http://localhost:3000/api/location';
+  
+  $.ajax({
+    url: my_url,
+    type: 'GET',
+    data: {
+      near: location
+    },
+    success: function(response){
+      //save data to local storage and then redirect to new page
+      localStorage.setItem('venue_data', JSON.stringify(response));
+      localStorage.setItem('location', location);
+      window.location.href = '/itinerary.html';
+    },
+    error: function(error){
+      console.log('Error: ' + error);
+    }
+  });
+}
