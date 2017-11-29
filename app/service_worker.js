cbf6d660f8cfa35aef6c1318211e23c522cccf00
Leon Hassan <leonhassan@hotmail.com>
2017-11-29 14:10:04 +0000

    updated service worker to follow best practices and use caching

--- app/service_worker.js
+++ app/service_worker.js
@@ -1,40 +1,73 @@
-self.addEventListener('install', function(event) {
+var cacheName = 'wandrAppShell';
+
+var myCache = [
+  '/',
+  '/index.html',
+  '/itinerary.html',
+  '/build/css/style.min.css',
+  '/build/fonts/ClinicaPro-Regular-webfont.woff',
+  '/build/fonts/ClinicaPro-Regular-webfont.woff2',
+  '/assets/brand/facebook.svg',
+  '/assets/brand/foursquare.png',
+  '/assets/brand/twitter.svg',
+  '/assets/wandr-logo.svg',
+  '/js/itinerary.js',
+  '/js/landing.js',
+  '/assets/arrow-left.svg',
+  '/assets/arrow-right.svg',
+  '/assets/edit_24.svg',
+  '/assets/genre_32.svg',
+  '/assets/pin-filled.svg',
+  '/assets/pin-unfilled.svg',
+  '/assets/refresh_32.svg',
+  '/assets/reset_32.svg',
+  '/assets/travel-node_active.svg',
+  '/assets/travel-node.svg'
+];
+
+// cache the application shell
+self.addEventListener('install', (event) => {
   event.waitUntil(
-    caches.open('resources').then(function(cache) {
-      return cache.addAll([
-        '/build/css/style.min.css',
-        '/build/fonts/ClinicaPro-Regular-webfont.woff',
-        '/build/fonts/ClinicaPro-Regular-webfont.woff2',
-        '/assets/brand/facebook.svg',
-        '/assets/brand/foursquare.png',
-        '/assets/brand/twitter.svg',
-        '/assets/wandr-logo.svg',
-        '/js/itinerary.js',
-        '/js/landing.js',
-        '/assets/arrow-left.svg',
-        '/assets/arrow-right.svg',
-        '/assets/edit_24.svg',
-        '/assets/genre_32.svg',
-        '/assets/pin-filled.svg',
-        '/assets/pin-unfilled.svg',
-        '/assets/refresh_32.svg',
-        '/assets/reset_32.svg',
-        '/assets/travel-node_active.svg',
-        '/assets/travel-node.svg'
-      ]);
+    caches.open(cacheName)
+    .then((cache) => {
+      return cache.addAll(myCache);
     })
   );
 });
 
-self.addEventListener('fetch', function(event) {
-  event.respondWith(
-    caches.match(event.request).then(function(response) {
-      return response || fetch(event.request);
-    })
-    .catch(function (err) {
-        // swallow error
+self.addEventListener('fetch', (event) => {
+  if (event.request.method === 'GET') {
+    event.respondWith(
+      caches.match(event.request).then((response) => {
+        if (response) {
+          return response;
+        }
+        return fetch(event.request).then((response) => {
+          return caches.open(cacheName).then((cache) => {
+            cache.put(event.request.url, response.clone());
+            return response;
+          });
+        });
+        // shorthand of above
+        //return response || fetchAndCache(event.request);
+      })
+    );
+  }
+});
 
-        //console.log(err.stack);
+// delete outdated caches in the activate event listener
+self.addEventListener('activate', (event) => {
+  var cacheWhitelist = [cacheName];
+
+  event.waitUntil(
+    caches.keys().then((cacheNames) => {
+      return Promise.all(
+        cacheNames.map((cacheName) => {
+          if (cacheWhitelist.indexOf(cacheName) === -1) {
+            return caches.delete(cacheName);
+          }
+        })
+      );
     })
   );
 });
